987696ce88fb608e626d20f6f2c73a44eced652c
californiansnowball <xxxm15010@gmail.com>
2023-08-05 00:21:55 +0300

    ffffffffffffffff

---
+++ lesson3.py
@@ -0,0 +1,12 @@
+numb = int(input('введите число'))
+if numb >= 1 and numb <= 9:
+    print('однозначное')
+if numb >= 10 and numb <= 99:
+    print('двузначное')
+if numb >= 100 and numb <= 1000:
+    print('трехзначное')
+
+
+
+
+
